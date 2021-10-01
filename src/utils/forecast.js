f985e1bda397fa6d9827f98e0ea892df129cfd9f
Ghermanj <ghermanjaramillo@yahoo.com>
2021-10-01 19:11:17 +0800

    Add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request');
+
 const forecast =(latitude,longitude,callback) => {
     const url='http://api.weatherstack.com/current?access_key=8155b80d7cb09b769a39a80223369a81&query='+ latitude + ',' + longitude + ''
     request({url, json: true}, (error, {body}) => {
@@ -7,8 +8,13 @@ const forecast =(latitude,longitude,callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+            console.log(body)
             callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +
-                ' degrees out. There is a ' + body.current.precip + '% chance of rain')
+                ' degrees out, it feels like ' + body.current.feelslike + ' degrees. There is a ' + body.current.precip +
+                '% chance of rain. Humidity is ' + body.current.humidity + '%.The ideal relative humidity for health ' +
+                'and comfort is somewhere between 30-50% according to Mayo Clinic. So 100 percent humidity might not mean ' +
+                'rain, but it does mean dew.' )
+
         }
     })
 }
